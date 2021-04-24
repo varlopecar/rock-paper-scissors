1cacd08bbd2eb2a6b3e50212fba389c42266edc9
Carlos Vargas <varlopecar@gmail.com>
2021-04-24 10:39:13 -0500

    Cuarto commit

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 'use strict'
 
-function playerSelection(){
-const selection = console.log('Choose: Rock, Paper, Scissors')
-    if(String(selection.toLowerCase()) === String('Rock'.toLowerCase())){
-        console.log('Rock')
-        selection = 1
-    } else if(String(selection.toLowerCase()) === String('Paper'.toLowerCase())){
-        console.log('Paper')
-        selection = 2
-    } else if(String(selection.toLowerCase()) === String('Scissors'.toLowerCase())){
-        console.log('Scissors')
-        selection = 3
-    }
-return selection
-}
-
-const selection = playerSelection()
-
 function computerPlay(){
 const randomNumber = Math.floor((Math.random()*3)+0)
     if(randomNumber === 0){
-        return 'rock'
+        return "rock"
     } else if(randomNumber === 1){
-        return 'paper'
+        return "paper"
     } else if(randomNumber === 2){
-        return 'scissors'
+        return "scissors"
     }
 }
+
+function playRound(playerSelection, computerPlay){
+    if(playerSelection === computerPlay){
+        return console.log(`It's a draw`)
+
+    } else if(playerSelection === "rock"){
+        if(computerPlay === "paper") 
+        return console.log('You lost')
+        if(computerPlay === "scissors") 
+        return console.log('You won')
+
+    } else if(playerSelection === "paper"){
+        if(computerPlay === "scissors") 
+        return console.log('You lost')
+        if(computerPlay === "rock") 
+        return console.log("You won")
+
+    } else if(playerSelection === "scissors"){
+        if(computerPlay === "rock") 
+        return console.log('You lost')
+        if(computerPlay === "paper") 
+        return console.log("You won")
+    }
+}
+
+const playerSelection = "rock"
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
