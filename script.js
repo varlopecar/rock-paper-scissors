190ce68796a24c9a6476bd6cec41bf8a18cb71d6
Carlos Vargas <varlopecar@gmail.com>
2021-09-05 20:26:52 -0500

    Nada

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
-'use strict'
+'use strict';
 
-const computerSelection = computerPlay()
+// const computerSelection = computerPlay();
 
 function playerSelection(){
-    const selection = prompt('Choose rock, paper or scissors')
-    return selection
+    let selection = '';
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach(button => button.addEventListener('click', function() {
+            selection = button.id;
+        }
+    ));
+    return console.log(selection);
 }
 
-function computerPlay(){
-const randomNumber = Math.floor((Math.random()*3)+0)
-    if(randomNumber === 0){
-        return "rock"
-    } else if(randomNumber === 1){
-        return "paper"
-    } else if(randomNumber === 2){
-        return "scissors"
-    }
-}
+console.log(playerSelection());
 
-function playRound(playerSelection, computerPlay){
-    if(playerSelection === computerPlay){
-        return "tie"
+// function computerPlay(){
+// const randomNumber = Math.floor((Math.random()*3)+0)
+//     if(randomNumber === 0){
+//         return 0
+//     } else if(randomNumber === 1){
+//         return 1
+//     } else if(randomNumber === 2){
+//         return 2
+//     }
+// }
 
-    } else if(playerSelection === "rock"){
-        if(computerPlay === "paper") 
-        return "lost"
-        if(computerPlay === "scissors") 
-        return "won"
+// function playRound(playerSelection, computerPlay){
+//     if(playerSelection === computerPlay){
+//         return "tie"
 
-    } else if(playerSelection === "paper"){
-        if(computerPlay === "scissors") 
-        return "lost"
-        if(computerPlay === "rock") 
-        return "won"
+//     } else if(playerSelection === "rock"){
+//         if(computerPlay === "paper") 
+//         return "lost"
+//         if(computerPlay === "scissors") 
+//         return "won"
 
-    } else if(playerSelection === "scissors"){
-        if(computerPlay === "rock") 
-        return "lost"
-        if(computerPlay === "paper") 
-        return "won"
-    }
-}
+//     } else if(playerSelection === "paper"){
+//         if(computerPlay === "scissors") 
+//         return "lost"
+//         if(computerPlay === "rock") 
+//         return "won"
+
+//     } else if(playerSelection === "scissors"){
+//         if(computerPlay === "rock") 
+//         return "lost"
+//         if(computerPlay === "paper") 
+//         return "won"
+//     }
+// }
 
 // function game(){
 //     for(let i=0; i<5; i++)
